684600df2925907b9231bb5c3c0f3849be009034
antonio <antoniorodriguezruiz2002@gmail.com>
2023-11-29 19:37:47 +0100

    render6

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd decide/
 
+python -m pip install -r requirements.txt
+
 cp local_settings.deploy.py local_settings.py
 ./manage.py createsuperuser --noinput
 ./manage.py collectstatic --noinput
